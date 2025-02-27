84f4320e7ab0d063dca3fd96907a9771baf68fcb
davymaciel <davymaciel700@gmail.com>
2025-02-27 11:22:14 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    return(
+      <View>
+        <Text style={{color: '#480082', fontSize: 25, margin: 10}}>Aplicação inicial</Text>       
+        <Text>Espaço Inicial da Aplicação</Text>
+        <Text>Aplicação inicial</Text>
+        
+        <Image 
+        source={{uri: 'https://burst.shopifycdn.com/photos/perfect-yellow-flower.jpg?width=1000&format=pjpg&exif=0&iptc=0'}}
+        style={{width: 300, height: 300 }}
+        />
+
+      </View>  
+                
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
