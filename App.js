c1ca9fb3739d0558348fabc66ba7dce31b11df68
davymaciel <davymaciel700@gmail.com>
2025-03-04 00:40:28 -0300

    estudos e ajustando estrutura

--- App.js
+++ App.js
@@ -4,17 +4,30 @@ import { View, Text, Image } from 'react-native';
 class App extends Component {
   render() {
     return(
-      <View>
-        <Text style={{color: '#480082', fontSize: 25, margin: 10}}>Aplicação inicial</Text>       
-        <Text>Espaço Inicial da Aplicação</Text>
-        <Text>Aplicação inicial</Text>
-        
+      <View>        
+        <Text style={{color: '#480082', fontSize: 25, margin: 10}}>Álbums em destaque</Text>       
         <Image 
-        source={{uri: 'https://burst.shopifycdn.com/photos/perfect-yellow-flower.jpg?width=1000&format=pjpg&exif=0&iptc=0'}}
-        style={{width: 300, height: 300 }}
+        source={{uri: 'https://cdn-images.dzcdn.net/images/cover/82db4c0f8e9412cafb1cd765b076d58c/500x500.jpg'}}
+        style={{width: 300, height: 300}}
         />
+        <Text>GNX</Text>
+        <Text>Kendrick Lamar</Text>
 
-      </View>  
+        <Image 
+        source={{uri: 'https://upload.wikimedia.org/wikipedia/en/6/6a/PartyNextDoor_and_Drake_-_Some_Sexy_Songs_4_U.png'}}
+        style={{width: 300, height: 300}}
+        />
+        <Text>$ome $exy $ongs 4 U</Text>
+        <Text>PartyNextDoor & Drake</Text>
+
+         <Image 
+        source={{uri: 'https://i.scdn.co/image/ab67616d0000b2737e7f1d0bdb2bb5a2afc4fb25'}}
+        style={{width: 300, height: 300}}
+        />
+        <Text>Hurry Up Tomorrow </Text>
+        <Text>The Weeknd</Text>
+      </View>
+        
                 
     );
   }
